c141929b32b9e601117faffdc870c9c484c95920
baiysh <baiyshbeksultanov1@gmail.com>
2023-11-20 19:10:42 +0600

    first commit

---
+++ src/common/fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCto8cZiDz4LxB2aqky6wHQKxA1RZI_-kQ",
+  authDomain: "spotify-6ad56.firebaseapp.com",
+  projectId: "spotify-6ad56",
+  storageBucket: "spotify-6ad56.appspot.com",
+  messagingSenderId: "862994877718",
+  appId: "1:862994877718:web:e89acb7fe3b99c3f6a5437",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
